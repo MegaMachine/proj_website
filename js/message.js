bea7dbd77e2af9b0d1fb4a591a63b4523b3fabd9
M3G4MACH1N3 <tolikdanylyuk@gmail.com>
2017-10-09 22:18:16 +0300

    BusGoAway

--- js/message.js
+++ js/message.js
@@ -2,7 +2,7 @@ $(function(){
     $('#btn').click(function(e) {
         console.log($("#email").val());
         console.log($("#textmessage").val());
-        e.preventDefault();
+
         $.ajax({
             url: "https://formspree.io/tolikdan@yahoo.com",
             method: "POST",
@@ -13,8 +13,12 @@ $(function(){
             dataType: "json"
             }).done(function() {
                 $('#info').html('Thank you!').css('color','#2aea57');
+                setTimeout(function(){
+                    $('#info').css('visibility','none');
+                },6000);
             }).fail(function(){
                 $('#info').html('Error!').css('color','#ed4444');
             });
+            e.preventDefault();
     });
 });
